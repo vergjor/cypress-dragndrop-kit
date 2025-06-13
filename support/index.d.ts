24b87daaa2303d2272bd189ead5302870ac29e81
Veronika Gjoreva <veronika.gjoreva@gmail.com>
2025-06-14 01:34:50 +0200

    Initial project setup

---
+++ support/index.d.ts
@@ -0,0 +1,22 @@
+declare namespace Cypress {
+    interface Chainable {
+        /**
+         * 
+         * @description Custom child command for dragging and dropping a chained element to a specified element location
+         * @param targetElement locator for the target element
+         * @example cy.get('[data-id="3"]').dragTo('[data-id="47"]');
+         * 
+         */
+        dragTo(targetElement: string): void;
+
+        /**
+         * 
+         * @description Custom command for dragging and dropping from one element location to another 
+         * @param sourceElement locator for the source element
+         * @param targetElement locator for the target element
+         * @example cy.dragAndDrop('[data-id="3"]', '[data-id="47"]');
+         * 
+         */
+        dragAndDrop(sourceElement: string, targetElement: string): void;
+    }
+}
---
+++ support/utils.ts
@@ -0,0 +1,8 @@
+import { Coordinate } from "./types"
+
+export const params = ({ x, y }: Coordinate, offset = 0) => ({
+    button: 0,
+    clientX: x + offset,
+    clientY: y + offset,
+    force: true,
+  });
