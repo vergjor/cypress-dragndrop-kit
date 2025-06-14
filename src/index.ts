5b0c7feaca8a2b658713ef398a446d953cff9b16
Veronika Gjoreva <veronika.gjoreva@gmail.com>
2025-06-14 15:55:48 +0200

    Resolve missing declaration file issue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
-import './commands';
+/// <reference types="cypress" />
+/// <reference path="./types.d.ts" />
 
-export {};
+import { DraggableOption, params } from "./utils";
+
+
+Cypress.Commands.add("dragTo", { prevSubject: 'element' }, (sourceElement, targetElement, options) => {
+  cy.get(targetElement).first().then(([target]) => {
+    cy.wrap(sourceElement).first().then(([source]) => {
+
+      let sourceCoordinates = source.getBoundingClientRect();
+      let targetCoordinates = target.getBoundingClientRect();
+
+      const isScrollingDown = targetCoordinates.top - sourceCoordinates.top > 0;
+      const isScrollingRight = targetCoordinates.right - sourceCoordinates.right > 0;
+
+      if (isScrollingDown || isScrollingRight) {
+        cy.get(targetElement).first().scrollIntoView();
+      } else {
+        const startX = sourceCoordinates.left + sourceCoordinates.width / 2;
+        const startY = sourceCoordinates.top + sourceCoordinates.height / 2;
+        sourceCoordinates = { ...sourceCoordinates, x: startX, y: startY };
+
+        const endX = targetCoordinates.left + targetCoordinates.width / 2;
+        const endY = targetCoordinates.top + targetCoordinates.height / 2;
+        targetCoordinates = { ...targetCoordinates, x: endX, y: endY };
+      }
+
+      cy.wrap(source)
+        .trigger("mousedown", params(sourceCoordinates))
+        .wait(options?.pressDelay ?? 0)
+        .trigger("mousemove", params(sourceCoordinates, 10));
+
+      cy.get("body")
+        .trigger("mousemove", params(targetCoordinates))
+        .wait(1000)
+        .trigger("mouseup", { force: true });
+    });
+  });
+});
+
+Cypress.Commands.add("dragAndDrop", (sourceElement: string, targetElement: string, options?: DraggableOption) => {
+  cy.get(sourceElement).dragTo(targetElement, options);
+});
